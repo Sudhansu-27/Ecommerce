36529e03de7e51555d8e8a4826811fabb42d0866
Sudhansu-27 <bariksudhansusekhar77@gmail.com>
2022-10-06 18:48:03 +0530

    Added Frontend

--- ecommerce/account/models.py
+++ ecommerce/account/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
-from ecommerce.orders.models import Order
+from orders.models import Order
 
 class CustomUser(AbstractUser):
 
     email = models.EmailField(blank=False, max_length=254, verbose_name="email address")
-    order = models.ForeignKey(Order , related_name='customuser' , on_delete=models.CASCADE)
+    order = models.ForeignKey(Order , related_name='customuser' ,null=True, blank=True, on_delete=models.CASCADE)
 
     USERNAME_FIELD = "username"   # e.g: "username", "email"
     EMAIL_FIELD = "email"         # e.g: "email", "primary_email"
